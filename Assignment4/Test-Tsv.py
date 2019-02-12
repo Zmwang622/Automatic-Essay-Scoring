bc968cd0ad0e3454ff5632c2835de259005a7eb1
Zhengming Wang <zmwang622@gmail.com>
2019-02-12 18:31:59 -0500

    Turned TSV file to filtered comments. Wow this took a long time.

--- Assignment4/Test-Tsv.py
+++ Assignment4/Test-Tsv.py
@@ -16,9 +16,24 @@ from sklearn.model_selection import train_test_split
 from nltk.sentiment import sentiment_analyzer
 from nltk.sentiment.sentiment_analyzer import SentimentAnalyzer
 
-stopwords = nltk.corpus.stopwords.words('english')
+
+
 training = pd.read_csv('drugLibTrain_raw.tsv', delimiter = '\t', encoding = 'utf-8')
 testing = pd.read_csv('drugLibTest_raw.tsv', delimiter = '\t', encoding = 'utf-8')
-columns = training.columns
 
-print(training.columns)
+training['commentsReview'] = training['commentsReview'].astype(str)
+raw_comments=""
+for index, row in training.iterrows():
+    raw_comments+= (row['commentsReview'])
+
+tokenized_comments = word_tokenize(raw_comments)
+
+stop_words = set(stopwords.words('english')) #About 150 stopwords
+
+filtered_comments = []
+for comment in tokenized_comments:
+    if comment not in stop_words:
+        filtered_comments.append(comment)
+
+print(filtered_comments)
+    
