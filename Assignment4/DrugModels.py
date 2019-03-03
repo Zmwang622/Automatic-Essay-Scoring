dbb3372fdbcbf49bf1955e489ec2259574751d43
Zhengming Wang <zmwang622@gmail.com>
2019-03-03 01:22:59 -0500

    Got it working!!! some how

--- Assignment4/DrugModels.py
+++ Assignment4/DrugModels.py
@@ -19,6 +19,9 @@ from test.test_threading_local import target
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import cohen_kappa_score
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 
 #Reading Data and other garbage
 stop_words = set(stopwords.words('english'))
@@ -41,20 +44,33 @@ def clean_text(text):
 
 
 #Vectorizing
-train_count_vect = CountVectorizer(analyzer=clean_text)
-test_count_vect = CountVectorizer(analyzer=clean_text)
-Train_counts = train_count_vect.fit_transform(train_data['commentsReview'])
-Test_counts = test_count_vect.fit_transform(test_data['commentsReview'])
+count_vect = CountVectorizer(ngram_range=(1,2),analyzer=clean_text, max_features=1)
+Train_counts = count_vect.fit_transform(train_data['commentsReview'])
+test_counts = count_vect.transform(test_data['commentsReview'])
 # print(X_counts.shape)
-train_feature_names = train_count_vect.get_feature_names()
+train_feature_names = count_vect.get_feature_names()
 train_labels= train_data['rating']
-train_label_names=[1,2,3,4,5,6,7,8,9,10]
-train_features=Train_counts.toarray()
+label_names=[1,2,3,4,5,6,7,8,9,10]
 
-test_feature_names = test_count_vect.get_feature_names()
+print(Train_counts.shape)
+print(test_counts.shape)
+
+test_feature_names = count_vect.get_feature_names()
 test_label=test_data['rating']
-test_label_names=[1,2,3,4,5,6,7,8,9,10]
-test_features=Test_counts.toarray()
+
+
+# Feature Extraction
+# selector = SelectKBest(score_func=chi2, k=7000)
+# Train_counts= selector.fit_transform(Train_counts,train_labels)
+# test_counts= selector.transform(test_counts)
+# print(Train_counts.shape)
+# print(test_counts.shape)
+
+
+# test_label_names=[1,2,3,4,5,6,7,8,9,10]
+train_features=Train_counts.toarray()
+test_features=test_counts.toarray()
+
 
 #Wow I can't believe I got this far. It's now time to do some Machine Learning classifications.
 #Gaussian Naive Bayes
@@ -62,4 +78,12 @@ gnb=GaussianNB()
 gnb_model = gnb.fit(train_features,train_labels)
 gnb_preds = gnb.predict(test_features)
 gnb_accuracy = accuracy_score(test_label,gnb_preds)
-print("Accuracy",gnb_accuracy)
+gnb_kappa = cohen_kappa_score(test_label,gnb_preds)
+gnb_qkappa = cohen_kappa_score(test_label,gnb_preds, weights="quadratic")
+
+lr = LogisticRegression(solver = 'lbfgs',multi_class="multinomial")
+lr_model = lr.fit(train_features,train_labels)
+lr_preds= lr.predict(test_features)
+lr_acc =accuracy_score(test_label, lr_preds)
+lr_kappa = cohen_kappa_score(test_label,gnb_preds)
+lr_qkappa = cohen_kappa_score(test_label,lr_preds, weights="quadratic")
